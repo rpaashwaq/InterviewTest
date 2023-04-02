7273c82e55ded06e3470251b3d0b123b176eaaba
Mohammed Ahmed <>
2023-04-02 15:35:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    
+    System.out.println("Hello world!");
+  }
+
+  /**
+   Write a function to accept two string parameters
+   and return true if all the letters in first string 
+   exist in second string
+  */
+  public static boolean compareStrings(String source, String target){
+    
+  }
+}
